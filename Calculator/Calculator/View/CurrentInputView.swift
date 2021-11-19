88d77320f2d50ea48d8014067f7ec64171d4fe25
JT <dd3557@naver.com>
2021-11-19 14:32:53 +0900

    feat: CurrentInputView 클래스 생성 및 이니셜라이저 오버라이드 코드 추가

---
+++ Calculator/Calculator/View/CurrentInputView.swift
@@ -0,0 +1,24 @@
+//
+//  CurrentInputView.swift
+//  Calculator
+//
+//  Created by 김진태 on 2021/11/19.
+//
+
+import UIKit
+
+class CurrentInputView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureView()
+    }
+    
+    private func configureView() {
+    }
+}
