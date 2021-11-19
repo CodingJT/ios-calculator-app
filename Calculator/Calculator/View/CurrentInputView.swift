6e9e9d4df9827d1b55a130cee70a9129acb46c7d
JT <dd3557@naver.com>
2021-11-19 14:40:48 +0900

    feat: CurrentInputView에 configureStackView 메소드 구현

--- Calculator/Calculator/View/CurrentInputView.swift
+++ Calculator/Calculator/View/CurrentInputView.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class CurrentInputView: UIView {
     
+    private let operatorTextLabel: UILabel = UILabel()
+    private let operandTextLabel: UILabel = UILabel()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureView()
@@ -20,5 +23,23 @@ class CurrentInputView: UIView {
     }
     
     private func configureView() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        configureStackView()
+    }
+    
+    private func configureStackView() {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        
+        self.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
     }
 }
