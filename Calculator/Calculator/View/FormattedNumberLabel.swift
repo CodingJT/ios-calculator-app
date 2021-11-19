ed47120cd7ff864e48743d571b504f9141a5d1dc
JT <dd3557@naver.com>
2021-11-19 22:21:33 +0900

    feat: 소수점을 2번 이상 포함하지 않도록 FormattedNumberLabel에 프로퍼티 추가 및 메소드 수정

--- Calculator/Calculator/View/FormattedNumberLabel.swift
+++ Calculator/Calculator/View/FormattedNumberLabel.swift
@@ -10,6 +10,7 @@ import UIKit
 @IBDesignable
 class FormattedNumberLabel: UILabel {
     private let zeroString = String(Int.zero)
+    private var isDecimalPointIncluded = false
     
     @IBInspectable private(set) var unformattedNumberText: String = "" {
         didSet {
@@ -29,11 +30,18 @@ class FormattedNumberLabel: UILabel {
     
     func appendNumberText(_ value: String) {
         var numberText = self.unformattedNumberText
+        let decimalPointCharacter = Character(".")
         
-        let signsString = "\(Operator.add.rawValue)\(Operator.subtract.rawValue)."
-        let decimalDigitsAndSignsSet = CharacterSet.decimalDigits.union(CharacterSet(charactersIn: signsString))
+        let signsString = "\(Operator.add.rawValue)\(Operator.subtract.rawValue)"
+        let signsAndDecimalPointSet = CharacterSet(charactersIn: "\(signsString)\(decimalPointCharacter)")
+        let decimalDigitsAndSignsWithDecimalPointSet = CharacterSet.decimalDigits.union(signsAndDecimalPointSet)
         
-        guard value.rangeOfCharacter(from: decimalDigitsAndSignsSet.inverted) == nil else { return }
+        guard value.rangeOfCharacter(from: decimalDigitsAndSignsWithDecimalPointSet.inverted) == nil else { return }
+        
+        if value.contains(decimalPointCharacter) {
+            guard isDecimalPointIncluded == false else { return }
+            isDecimalPointIncluded = true
+        }
         
         numberText.append(value)
         self.unformattedNumberText = numberText
@@ -41,5 +49,6 @@ class FormattedNumberLabel: UILabel {
     
     func setNumberTextToZero() {
         unformattedNumberText = zeroString
+        isDecimalPointIncluded = false
     }
 }
