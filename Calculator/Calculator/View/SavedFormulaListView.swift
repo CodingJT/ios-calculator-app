c9ef79277761c21048c8992b669d5bc04c7c381b
JT <dd3557@naver.com>
2021-11-21 21:54:04 +0900

    feat: SavedFormulaListView 클래스 생성

---
+++ Calculator/Calculator/View/SavedFormulaListView.swift
@@ -0,0 +1,26 @@
+//
+//  SavedFormulaListView.swift
+//  Calculator
+//
+//  Created by 김진태 on 2021/11/21.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class SavedFormulaListView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        
+    }
+}
